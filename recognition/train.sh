ae7dcd8bbcb467f281c4d21331762e1d9ea550ff
duanxiongwen <duanxiongwen@gbcom.com.cn>
2020-08-06 11:08:53 +0800

    dualven add new

---
+++ recognition/train.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES='0' python -u train.py --network r100 --loss arcface --dataset emore
---
+++ src/data/pack.sh
@@ -0,0 +1,2 @@
+#进行打包，共2000个
+python lfw2pack.py --data-dir ../../datasets/train3 --output ../../datasets/train3/lfw.bin --num_samepairs 1132
